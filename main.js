53b01d6a0df97831a1628fd80c5e962b2c342747
Jonathan de Jong <jonathandejong02@gmail.com>
2017-09-21 20:24:39 +0200

    added version requirement

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('events').EventEmitter.defaultMaxListeners = 100;
+require('events').EventEmitter.defaultMaxListeners = 100;if (parseInt(/v(\d+)/.exec(process.version)[1]) < 8) throw new Error("NODE VERSION UNDER 8");
 const express = require('express');
 const http = require('http');
 const url = require('url');
